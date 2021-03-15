358791094d1ef3b4b32e567260e162b5579cd423
Maël Valais <mael@vls.dev>
2021-03-15 11:09:47 +0100

    Write a good --help

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -23,6 +25,7 @@ import (
 	"github.com/gohugoio/hugo/hugofs"
 	"github.com/gohugoio/hugo/hugolib"
 	"github.com/gohugoio/hugo/resources/page"
+	"github.com/mgutz/ansi"
 	"github.com/sethgrid/gencurl"
 	"github.com/spf13/jwalterweatherman"
 	"github.com/spf13/viper"
@@ -36,7 +39,127 @@ var (
 	debug      = flag.Bool("debug", false, "Print debug information such as the HTTP requests that are being made in curl format.")
 )
 
+const help = `{{ if .Extended -}}
+hudevto allows you to synchronize your Hugo posts with your DEV articles. The
+synchronization is one way (Hugo to DEV). A Hugo post is only pushed when a
+change is detected. When pushed to DEV, the Hugo article is applied two
+transformations: the relative image links are absolutified, and the Hugo tags
+are turned into Liquid tags.
+{{ end }}
+{{ section "USAGE" }}
+  hudevto [OPTION] (preview|diff) POST
+  hudevto [OPTION] status [POST]
+  hudevto [OPTION] push [POST]
+  hudevto [OPTION] devto list
+{{- if .Extended }}
+
+{{ section "DESCRIPTION" }}
+In order to operate, hudevto requires you to have your DEV account configured
+with "Publish to DEV Community from your blog's RSS". You can configure that at
+{{ url "https://dev.to/settings/extensions" }}. DEV will create a draft article for
+every Hugo post that you have published on your blog. For example, Let us
+imagine that your Hugo blog layout is:
+
+    .
+    └── content
+       ├── brick-chest.md
+       ├── cloth-impossible.md
+       └── powder-farmer.md
+
+After configuring the RSS feed of your blog at {{ url "https://maelvls.dev/index.xml" }},
+DEV should create one draft article per post. You can check that these articles
+have been created on DEV with:
+
+    {{ cmd "hudevto devto list" }}
+    {{ out "386001: unpublished at https://dev.to/maelvls/brick-chest/edit" }}
+    {{ out "386002: unpublished at https://dev.to/maelvls/cloth-impossible/edit" }}
+    {{ out "386003: unpublished at https://dev.to/maelvls/powder-farmer/edit" }}
+
+The next step is to map each article that you want to sync to DEV. Let us see
+the state of the mapping:
+
+    {{ cmd "hudevto status" }}
+    {{ out "error: ./content/brick-chest.md: missing devtoId field in front matter, might be 386001: https://dev.to/maelvls/brick-chest/edit" }}
+    {{ out "error: ./content/cloth-impossible.md: missing devtoId field in front matter, might be 386002: https://dev.to/maelvls/cloth-impossible/edit" }}
+    {{ out "error: ./content/powder-farmer.md: missing devtoId field in front matter, might be 386003: https://dev.to/maelvls/powder-farmer/edit" }}
+
+At this point, you need to open each of your Hugo post and add some fields to
+their front matters. For example, in ./content/brick-chest.md, we add this:
+
+    devtoId: 386001       {{ grey "# This is the DEV ID as seen in hudevto devto list" }}
+    devtoPublished: true  {{ grey "# When false, the DEV article will stay a draft" }}
+    devtoSkip: false      {{ grey "# When true, hudevto will ignore this post." }}
+
+The status should have changed:
+
+    {{ cmd "hudevto status" }}
+    {{ out "info: ./content/brick-chest.md will be pushed published to https://dev.to/maelvls/brick-chest/edit" }}
+    {{ out "info: ./content/cloth-impossible.md will be pushed published to https://dev.to/maelvls/cloth-impossible/edit" }}
+    {{ out "info: ./content/powder-farmer.md will be pushed published to https://dev.to/maelvls/powder-farmer/edit" }}
+
+Finally, you can push to DEV:
+
+    {{ cmd "hudevto push" }}
+    {{ out "success: ./content/brick-chest.md pushed to https://dev.to/maelvls/brick-chest-2588" }}
+    {{ out "success: ./content/cloth-impossible.md pushed to https://dev.to/maelvls/cloth-impossible-95dc" }}
+    {{ out "success: ./content/powder-farmer.md pushed to https://dev.to/maelvls/powder-farmer-6a18" }}
+{{ end }}
+
+{{ section "COMMANDS" }}
+  hudevto status [POST]
+      Shows the status of each post (or of a single post). The status shows
+      whether it is mapped to a DEV article and if a push is required when the
+      Hugo post has changes that are not on DEV yet.
+  hudevto preview POST
+      Displays a Markdown preview of the Hugo post that has been converted into
+      the DEV article Markdown format. You can use this command to check that
+      the tranformations were correctly applied.
+  hudevto diff POST
+      Displays a diff between the Hugo post and the DEV article. It is useful
+      when you want to see what changes will be pushed.
+  hudevto push [POST]
+      Pushes the given Hugo Markdown post to DEV. If no post is given, then
+      all posts are pushed.
+  hudevto devto list
+      Lists all the articles you have on your DEV account.
+
+{{ if not .Extended -}}
+More help is available with the command {{ yel "hudevto help" }}.
+
+{{ end -}}
+
+{{ section "OPTIONS" }}
+`
+
 func main() {
+
+	printHelp := func(extended bool) func() {
+		return func() {
+			t, err := template.New("test").Funcs(map[string]interface{}{
+				"section": ansi.ColorFunc("black+hb"),
+				"url":     ansi.ColorFunc("white+u"),
+				"grey":    ansi.ColorFunc("white+d"),
+				"yel":     ansi.ColorFunc("yellow"),
+				"cmd": func(cmd string) string {
+					return ansi.ColorFunc("white+d")("% ") + ansi.ColorFunc("yellow+b")(cmd)
+				},
+				"out": ansi.ColorFunc("white+d"),
+			}).Parse(help)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = t.Execute(os.Stderr, struct{ Extended bool }{
+				Extended: extended,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+			flag.PrintDefaults()
+		}
+	}
+	flag.Usage = printHelp(false)
+
 	flag.Parse()
 	logutil.EnableDebug = *debug
 
@@ -50,42 +173,59 @@ func main() {
 
 	switch flag.Arg(0) {
 	case "push":
-		err := PushArticlesFromHugoToDevto(filepath.Clean(*rootDir), flag.Arg(1), false, false, apiKey)
+		err := PushArticlesFromHugoToDevto(filepath.Clean(*rootDir), flag.Arg(1), false, false, false, apiKey)
 		if err != nil {
 			logutil.Errorf(err.Error())
 			os.Exit(1)
 		}
 	case "preview":
-		err := PushArticlesFromHugoToDevto(filepath.Clean(*rootDir), flag.Arg(1), true, false, apiKey)
+		err := PushArticlesFromHugoToDevto(filepath.Clean(*rootDir), flag.Arg(1), true, false, true, apiKey)
 		if err != nil {
 			logutil.Errorf(err.Error())
 			os.Exit(1)
 		}
 	case "diff":
-		err := PushArticlesFromHugoToDevto(filepath.Clean(*rootDir), flag.Arg(1), false, true, apiKey)
+		err := PushArticlesFromHugoToDevto(filepath.Clean(*rootDir), flag.Arg(1), false, true, true, apiKey)
 		if err != nil {
 			logutil.Errorf(err.Error())
+			os.Exit(1)
+		}
+	case "status":
+		err := PushArticlesFromHugoToDevto(filepath.Clean(*rootDir), flag.Arg(1), false, false, true, apiKey)
+		if err != nil {
+			logutil.Errorf(err.Error())
+			os.Exit(1)
+		}
+	case "devto":
+		if flag.Arg(1) == "list" {
+			err := PrintDevtoArticles(apiKey)
+			if err != nil {
+				logutil.Errorf(err.Error())
+				os.Exit(1)
+			}
+		} else {
+			logutil.Errorf("usage: hudevto devto list")
 			os.Exit(1)
 		}
 	case "list":
-		err := PrintDevtoArticles(apiKey)
-		if err != nil {
-			logutil.Errorf(err.Error())
-			os.Exit(1)
-		}
+		logutil.Errorf("did you mean 'list'? Try the command:\n    hudevto devto list")
+		os.Exit(1)
+	case "help":
+		printHelp(true)()
+		os.Exit(0)
+	case "":
+		logutil.Errorf("no command has been given.")
+		printHelp(false)()
+		os.Exit(1)
 	default:
 		logutil.Errorf("unknown command '%s'.", flag.Arg(0))
-		fmt.Fprintf(os.Stderr, heredoc.Doc(`
-			Usage:
-			  hudevto preview
-			  hudevto push
-			  hudevto list
-		`))
+		printHelp(false)()
+		os.Exit(1)
 	}
 }
 
 // Updates all articles if pathToArticle is left empty.
-func PushArticlesFromHugoToDevto(rootDir, pathToArticle string, showMarkdown, showDiff bool, apiKey string) error {
+func PushArticlesFromHugoToDevto(rootDir, pathToArticle string, showMarkdown, showDiff, dryRun bool, apiKey string) error {
 	conf, err := loadHugoConfig(rootDir)
 	if err != nil {
 		return err
@@ -162,9 +302,29 @@ func PushArticlesFromHugoToDevto(rootDir, pathToArticle string, showMarkdown, sh
 			continue
 		}
 
-		published := false
-		publishedRaw, err := page.Param("devtoPublished")
-		if publishedRaw == nil {
+		devtoSkip := false
+		devtoSkipRaw, err := page.Param("devtoSkip")
+		if devtoSkipRaw != nil && err == nil {
+			var ok bool
+			devtoSkip, ok = devtoSkipRaw.(bool)
+			if !ok {
+				logutil.Errorf("%s: field devtoSkip is expected to be a boolean, got '%T'",
+					logutil.Gray(pathToMD),
+					devtoSkipRaw,
+				)
+				continue
+			}
+		}
+		if devtoSkip {
+			logutil.Debugf("%s: field devtoSkip is true, skipping this post.",
+				logutil.Gray(pathToMD),
+			)
+			continue
+		}
+
+		devtoPublished := false
+		devtoPublishedRaw, err := page.Param("devtoPublished")
+		if devtoPublishedRaw == nil {
 			logutil.Errorf("%s: missing devtoPublished field",
 				logutil.Gray(pathToMD),
 			)
@@ -172,23 +332,23 @@ func PushArticlesFromHugoToDevto(rootDir, pathToArticle string, showMarkdown, sh
 		}
 		if err == nil {
 			var ok bool
-			published, ok = publishedRaw.(bool)
+			devtoPublished, ok = devtoPublishedRaw.(bool)
 			if !ok {
 				logutil.Errorf("%s: field devtoPublished is expected to be a boolean, got '%T'",
 					logutil.Gray(pathToMD),
-					publishedRaw,
+					devtoPublishedRaw,
 				)
 				continue
 			}
 		}
 
-		idRaw, err := page.Param("devtoId")
-		if err != nil || idRaw == nil {
+		devtoIdRaw, err := page.Param("devtoId")
+		if err != nil || devtoIdRaw == nil {
 			if art, ok := articlesTitleMap[page.Title()]; ok {
 				logutil.Errorf("%s missing devtoId field in front matter, might be %s: %s",
 					logutil.Gray(pathToMD),
 					logutil.Green(strconv.Itoa(int(art.ID))),
-					logutil.Yel(addEditSegment(art.URL.String(), published)),
+					logutil.Yel(addEditSegment(art.URL.String(), devtoPublished)),
 				)
 			} else {
 				logutil.Errorf("%s missing devtoId field in front matter and title cannot be found on your devto account",
@@ -197,28 +357,28 @@ func PushArticlesFromHugoToDevto(rootDir, pathToArticle string, showMarkdown, sh
 			}
 			continue
 		}
-		id, ok := idRaw.(int)
+		devtoId, ok := devtoIdRaw.(int)
 		if !ok {
 			logutil.Errorf("%s: field devtoId is expected to be an integer, got '%T'",
 				logutil.Gray(pathToMD),
-				idRaw,
+				devtoIdRaw,
 			)
 			continue
 		}
 
-		article, found := articlesIdMap[id]
+		article, found := articlesIdMap[devtoId]
 		if !found {
 			if art, ok := articlesTitleMap[page.Title()]; ok {
 				logutil.Errorf("%s: devtoId %s is unknown but title matches devtoId %s: %s",
 					logutil.Gray(pathToMD),
-					logutil.Red(strconv.Itoa(id)),
+					logutil.Red(strconv.Itoa(devtoId)),
 					logutil.Green(strconv.Itoa(int(art.ID))),
-					logutil.Yel(addEditSegment(art.URL.String(), published)),
+					logutil.Yel(addEditSegment(art.URL.String(), devtoPublished)),
 				)
 			} else {
 				logutil.Errorf("%s: devtoId %s is unknown and title cannot be found in your devto account",
 					logutil.Gray(pathToMD),
-					logutil.Red(strconv.Itoa(id)),
+					logutil.Red(strconv.Itoa(devtoId)),
 				)
 			}
 			continue
@@ -237,7 +397,7 @@ func PushArticlesFromHugoToDevto(rootDir, pathToArticle string, showMarkdown, sh
 				logutil.Cyan("+++"),
 				logutil.Cyan("- ")+logutil.Red(article.Title),
 				logutil.Cyan("+ ")+logutil.Green(page.Title()),
-				logutil.Yel(addEditSegment(article.URL.String(), published)),
+				logutil.Yel(addEditSegment(article.URL.String(), devtoPublished)),
 			))
 			continue
 		}
@@ -275,7 +435,7 @@ func PushArticlesFromHugoToDevto(rootDir, pathToArticle string, showMarkdown, sh
 			`,
 			page.Title(),
 			page.Description(),
-			published,
+			devtoPublished,
 			strings.Join(page.Keywords(), ", "),
 			page.Date().UTC().Format("20060102T15:04Z"),
 			strings.Join([]string{}, ", "),
@@ -294,11 +454,11 @@ func PushArticlesFromHugoToDevto(rootDir, pathToArticle string, showMarkdown, sh
 			return nil
 		}
 
-		existing, err := GetArticle(httpClient, id)
+		existing, err := GetArticle(httpClient, devtoId)
 		if err != nil {
 			logutil.Errorf("%s: fetching devto id %s: %s",
 				logutil.Gray(pathToMD),
-				logutil.Yel(strconv.Itoa(id)),
+				logutil.Yel(strconv.Itoa(devtoId)),
 				err,
 			)
 			continue
@@ -316,8 +476,24 @@ func PushArticlesFromHugoToDevto(rootDir, pathToArticle string, showMarkdown, sh
 			continue
 		}
 
+		if dryRun {
+			publishedStr := logutil.Red("unpublished")
+			if devtoPublished {
+				publishedStr = logutil.Green("published")
+			}
+			fmt.Printf("%s: %s will be pushed %s to %s (devtoId: %d, devtoPublished: %t)\n",
+				logutil.Yel("info"),
+				logutil.Gray(pathToMD),
+				publishedStr,
+				logutil.Yel(addEditSegment(article.URL.String(), devtoPublished)),
+				article.ID,
+				devtoPublished,
+			)
+			continue
+		}
+
 	Update:
-		art, err := UpdateArticle(httpClient, id, Article{BodyMarkdown: content})
+		art, err := UpdateArticle(httpClient, devtoId, Article{BodyMarkdown: content})
 		switch {
 		case isTooManyRequests(err):
 			// As per https://docs.forem.com/api/#operation/updateArticle,
@@ -327,23 +503,23 @@ func PushArticlesFromHugoToDevto(rootDir, pathToArticle string, showMarkdown, sh
 		case err != nil:
 			logutil.Errorf("%s: updating devto id %s: %s",
 				logutil.Gray(pathToMD),
-				logutil.Yel(strconv.Itoa(id)),
+				logutil.Yel(strconv.Itoa(devtoId)),
 				err,
 			)
 			continue
 		}
 
 		publishedStr := logutil.Red("unpublished")
-		if published {
+		if devtoPublished {
 			publishedStr = logutil.Green("published")
 		}
 		fmt.Printf("%s: %s pushed %s to %s (devtoId: %d, devtoPublished: %t)\n",
 			logutil.Green("success"),
 			logutil.Gray(pathToMD),
 			publishedStr,
-			logutil.Yel(addEditSegment(art.URL.String(), published)),
+			logutil.Yel(addEditSegment(art.URL.String(), devtoPublished)),
 			art.ID,
-			published,
+			devtoPublished,
 		)
 	}
 	return nil
